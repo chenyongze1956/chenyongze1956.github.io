68ddd7b13eb4a0a74b2429b281ffed8337f04665
chenyongze1956 <kimtony19881218@gmail.com>
2025-12-13 22:09:49 +0800

    test
    test

---
+++ StarMansion.js
@@ -0,0 +1,4 @@
+// StarMansion.js
+const StarMansion = ["角", "亢", "氐", "房", "心", "尾", "箕", "斗", "牛", "女", "虚", "危", "室", "壁", "奎", "娄", "胃", "昴", "毕", "觜", "参", "井", "鬼", "柳", "星", "张", "翼", "轸"];
+const START_MANSION_INDEX = 11; 
+export { StarMansion, START_MANSION_INDEX };
